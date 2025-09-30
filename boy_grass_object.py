08c57368a32c577a24c1c42037f2aaa919f58012
dkssud05 <hyeonuson51@gmail.com>
2025-09-30 19:23:42 +0900

    ball 클래스로 선언

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,14 @@
 from pico2d import *
 import random
 
+class ball:
+    def __init__(self):
+        self.image = load_image('ball21x21.png')
+    def update(self):
+        pass
+    def draw(self):
+        self.image.clip_draw(0, 0, 21, 21, 400, 90)
+
 class Boy:
     def __init__(self):
         self.x = random.randint(100, 700)
